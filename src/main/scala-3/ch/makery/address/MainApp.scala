0bcb6da3cf8a4123c188e121fb93d00e626c4978
Swee Shi Yi <23093206@imail.sunway.edu.my>
2025-06-24 15:58:42 +0800

    practical 01/07/2025

--- src/main/scala-3/ch/makery/address/MainApp.scala
+++ src/main/scala-3/ch/makery/address/MainApp.scala
@@ -1,16 +1,38 @@
 package ch.makery.address
 
+import ch.makery.address.model.Person
 import javafx.fxml.FXMLLoader
 import javafx.scene as jfxs
 import scalafx.Includes.*
 import scalafx.application.JFXApp3
 import scalafx.application.JFXApp3.PrimaryStage
+import scalafx.beans.property.StringProperty
+import scalafx.collections.ObservableBuffer
 import scalafx.scene.Scene
 
 object MainApp extends JFXApp3:
 
   //Window Root Pane
   var roots: Option[scalafx.scene.layout.BorderPane] = None
+
+  /**
+   * The data as an observable list of Persons.
+   */
+  val personData = new ObservableBuffer[Person]()
+
+  /**
+   * Constructor
+   */
+  personData += new Person("Hans", "Muster")
+  personData += new Person("Ruth", "Mueller")
+  personData += new Person("Heinz", "Kurz")
+  personData += new Person("Cornelia", "Meier")
+  personData += new Person("Werner", "Meyer")
+  personData += new Person("Lydia", "Kunz")
+  personData += new Person("Anna", "Best")
+  personData += new Person("Stefan", "Meier")
+  personData += new Person("Martin", "Mueller")
+
 
   override def start(): Unit =
     // transform path of RootLayout.fxml to URI for resource location.
@@ -37,3 +59,17 @@ object MainApp extends JFXApp3:
     loader.load()
     val roots = loader.getRoot[jfxs.layout.AnchorPane]
     this.roots.get.center = roots
+
+  val stringA = new StringProperty("hello")
+  val stringB = new StringProperty("sunway")
+  val stringC = new StringProperty("sunway")
+
+  stringA.onChange { (_, oldValue, newValue) =>
+    println(s"stringA changed from $oldValue to $newValue")
+  }
+
+  stringA.onChange { (_, _, _) =>
+    println(s"stringA has change!!!")
+  }
+
+  stringA.value = "world"
